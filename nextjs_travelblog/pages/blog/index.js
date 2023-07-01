4f5ad654ba19d6eef6195be067ff37ba31080ed7
Paul Onyebuchi <paulemi@live.com>
2023-07-01 17:09:03 +0100

    Fixed data fetching from wordpress
    Fixed code for data fetching from wordpress graphql plugin

--- nextjs_travelblog/pages/blog/index.js
+++ nextjs_travelblog/pages/blog/index.js
@@ -1,5 +1,31 @@
-export default function BlogHome() {
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+import siteHeader from '@/components/siteHeader';
+import { getAllPosts } from '@/lib/posts';
+
+
+export async function getStaticProps() {
+    const allPosts = await getAllPosts();
+
+    return {
+        props: {
+            allPosts: allPosts,
+        },
+        
+    }
+
+}
+
+export default function BlogHome({ allPosts }) {
     return (
-        <h1>This is our blog index page</h1>
+        <>
+            <Head>
+                <title>Blog</title>
+            </Head>
+            <div className="h-[50vh] min-h-[20rem] bg-[url('/home-bg.jpg') relative]">
+                <div className="absolute bg-slate-900 inset-0"></div>
+            </div>
+        </>
     );
 }
